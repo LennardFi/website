444996ab1c63842974ccc9c7d40c0a09a6c673d0
LennardFi <info@lennard-fickus.de>
2022-12-02 19:41:35 +0100

    updated portfolio page content

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,3 +1,6 @@
+import Img_Seagull from "../../content/portfolio/animals/DSCF3307-edited.jpg"
+import Img_DogAtTheSea from "../../content/portfolio/animals/DSCF4577-edited.jpg"
+import Img_DogOnTrack from "../../content/portfolio/animals/DSCF7666-edited.jpg"
 import Img_BlackWomanInMarburg from "../../content/portfolio/portraits/DSCF7225-edited.jpg"
 import Img_GuitarPlayerInMarburg from "../../content/portfolio/portraits/DSCF7268-edited.jpg"
 import Img_ManInMarburg from "../../content/portfolio/portraits/DSCF7473-edited.jpg"
@@ -5,9 +8,6 @@ import Img_TrainInRotterdam from "../../content/portfolio/street-photography/DSC
 import Img_CoupleInAmsterdam from "../../content/portfolio/street-photography/DSCF5408-edited.jpg"
 import Img_ManAtTheBeach from "../../content/portfolio/street-photography/DSCF5648-edited.jpg"
 import Img_OberstadtPassageInMarburg from "../../content/portfolio/street-photography/DSCF6721-edited.jpg"
-import Img_Seagull from "../../content/portfolio/animals/DSCF3307-edited.jpg"
-import Img_DogOnTrack from "../../content/portfolio/animals/DSCF7666-edited.jpg"
-import Img_DogAtTheSea from "../../content/portfolio/animals/DSCF4577-edited.jpg"
 import ContentGrid from "../components/ContentGrid/ContentGrid"
 import ContentGridCell from "../components/ContentGrid/ContentGridCell"
 import LocalizedLabel from "../components/LocalizedLabel"
@@ -36,12 +36,13 @@ const Portfolio = () => {
                 <ContentGridCell image={Img_GuitarPlayerInMarburg} />
             </ContentGrid>
             <h2>
-                <LocalizedLabel 
+                <LocalizedLabel
                     de="Straßenfotografie"
-                    en="Street Photography" />
+                    en="Street Photography"
+                />
             </h2>
             <ContentGrid
-                columns={[1, 2, 4, 4]}
+                columns={[1, 2, 2, 2]}
                 gap={2}
                 style={{
                     marginTop: "0",
@@ -53,10 +54,7 @@ const Portfolio = () => {
                 <ContentGridCell image={Img_OberstadtPassageInMarburg} />
             </ContentGrid>
             <h2>
-                <LocalizedLabel
-                    de="Haustiere"
-                    en="Pets"
-                />
+                <LocalizedLabel de="Haustiere" en="Pets" />
             </h2>
             <ContentGrid
                 columns={[1, 2, 2, 2]}
@@ -69,10 +67,7 @@ const Portfolio = () => {
                 <ContentGridCell image={Img_DogAtTheSea} />
             </ContentGrid>
             <h2>
-                <LocalizedLabel
-                    de="Natur"
-                    en="Nature"
-                />
+                <LocalizedLabel de="Natur" en="Nature" />
             </h2>
             <ContentGrid
                 columns={[1, 1, 1, 1]}
