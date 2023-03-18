0ccb29bb3ceb57f90e8144c7a4e2a67adbc5cf16
LennardFi <info@lennard-fickus.de>
2023-03-18 02:29:00 +0100

    removed unused config

--- src/next.config.js
+++ src/next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
     reactStrictMode: true,
     assetPrefix: "/",
     swcMinify: true,
-    devIndicators: {
-        buildActivity: true,
-        buildActivityPosition: "top-left",
-    },
 }
 
 module.exports = nextConfig
