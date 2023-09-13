bdfc14239484ef6f4e81ff96c4a0791d83dc35b4
LennardFi <info@lennard-fickus.de>
2023-09-14 01:02:37 +0200

    fixed deploy error; added script output

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+# prepare website
+echo Prepare...
 git pull
+nvm use stable
+
+# build website
+echo Building website...
 npm run build
+
+# start website
+echo Starting website...
 npm run start
