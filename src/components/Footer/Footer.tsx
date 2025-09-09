d9b9794e03c47eb571aa8587ed6390b0fdaa9425
LennardFi <info@lennard-fickus.de>
2025-09-09 22:12:42 +0200

    fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,66 +4,66 @@ import { FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa"
 import styles from "./Footer.module.scss"
 
 interface FooterProps {
-    dictionary: Website.I18n.Dictionary
-    lang: Website.I18n.Language
+	dictionary: Website.I18n.Dictionary
+	lang: Website.I18n.Language
 }
 
 const Footer = ({ dictionary, lang }: FooterProps) => {
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.container}>
-                <ul className={styles.links}>
-                    <li>
-                        <Link href={`${lang}/privacy`}>
-                            {dictionary.footer.link_privacy}
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href={`/${lang}/imprint`}>
-                            {dictionary.footer.link_imprint}
-                        </Link>
-                    </li>
-                </ul>
-                <ul className={styles.links}>
-                    <li>
-                        <Link href={`/${lang}/links`}>
-                            {dictionary.footer.link_links}
-                        </Link>
-                    </li>
-                </ul>
-                <hr />
-                <ul className={styles.socialMedia}>
-                    <li>
-                        <a
-                            href="https://www.instagram.com/lennardfi/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FaInstagram />
-                        </a>
-                    </li>
-                    <li>
-                        <Link
-                            href="https://www.tiktok.com/@lennardfi"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FaTiktok />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="https://www.youtube.de/@lennardfi"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FaYoutube />
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    )
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.container}>
+				<ul className={styles.links}>
+					<li>
+						<Link href={`/${lang}/privacy`}>
+							{dictionary.footer.link_privacy}
+						</Link>
+					</li>
+					<li>
+						<Link href={`/${lang}/imprint`}>
+							{dictionary.footer.link_imprint}
+						</Link>
+					</li>
+				</ul>
+				<ul className={styles.links}>
+					<li>
+						<Link href={`/${lang}/links`}>
+							{dictionary.footer.link_links}
+						</Link>
+					</li>
+				</ul>
+				<hr />
+				<ul className={styles.socialMedia}>
+					<li>
+						<a
+							href="https://www.instagram.com/lennardfi/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaInstagram />
+						</a>
+					</li>
+					<li>
+						<Link
+							href="https://www.tiktok.com/@lennardfi"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaTiktok />
+						</Link>
+					</li>
+					<li>
+						<Link
+							href="https://www.youtube.de/@lennardfi"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FaYoutube />
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
